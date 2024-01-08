0b6c4eef030001530ffcf003fc9d1b6ad286383e
mehedi-hasan <mehediinform@gmail.com>
2024-01-09 02:38:53 +0600

    2nd commit Time Picker Dialog ........

--- app/src/main/java/com/example/timepickerdialog/MainActivity.java
+++ app/src/main/java/com/example/timepickerdialog/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.timepickerdialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonShow;
+    private TextView textView;
+
+    private TimePickerDialog timePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        buttonShow = findViewById(R.id.button_Id);
+        textView = findViewById(R.id.textView_Id);
+
+        buttonShow.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        TimePicker timePicker = new TimePicker(this);
+
+        int currentHour = timePicker.getCurrentHour();
+        int currentMinute = timePicker.getCurrentMinute();
+
+
+        timePickerDialog = new TimePickerDialog(MainActivity.this,
+
+                new TimePickerDialog.OnTimeSetListener(){
+
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        textView.setText(hourOfDay +":"+ minute);
+                    }
+                },currentHour,currentMinute,true
+
+        );
+        timePickerDialog.show();
+
     }
 }
